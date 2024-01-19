44d32dac4954cf04898a319d30e82c50cd1fffef
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-01-19 13:42:03 -0600

    lab1 p1

---
+++ lab1/problem_1_13.m
@@ -0,0 +1,14 @@
+% real number in the range (0, 30)
+rand() * 30
+
+% real number in the range (10, 100)
+rand() * 90 + 10
+
+% integer in the inclusive range from 1 to 20
+randi([1, 20])
+
+% integer in the inclusive range from 0 to 20
+randi([0, 20])
+
+% integer in the inclusive range from 30 to 80
+randi([30, 80])
