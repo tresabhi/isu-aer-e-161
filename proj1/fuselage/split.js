3ecb372d658c744e63fcb896d7adbcbd24390d69
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2024-04-14 18:24:52 -0500

    feat: aaaaaaaaaaaa

--- proj1/fuselage/split.js
+++ proj1/fuselage/split.js
@@ -5,9 +5,10 @@ console.log(meshData.length);
 
 meshData.forEach((mesh, meshIndex) => {
   const vertices = mesh.indices.map((index) => mesh.vertices[index]);
+  let lines = vertices
+    .map((v) => `${v.join(",")}`)
+    .filter((value, index, array) => array.indexOf(value) === index);
+  lines = [...lines, lines[0]];
 
-  writeFile(
-    `proj1/fuselage/generated/${meshIndex}.txt`,
-    vertices.map((v) => `${v.join(",")}`).join("\n")
-  );
+  writeFile(`proj1/fuselage/generated/${meshIndex}.txt`, lines.join("\n"));
 });
